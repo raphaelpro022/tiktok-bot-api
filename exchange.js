0508bd0656874d8d2539066e13670464590265f5
raphaelpro022 <raphael.pro022@gmail.com>
2025-08-28 19:21:24 +0200

    Create exchange.js

---
+++ exchange.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const { code } = req.body;
+
+  const response = await fetch("https://open.tiktokapis.com/v2/oauth/token/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      client_key: process.env.TIKTOK_CLIENT_KEY,
+      client_secret: process.env.TIKTOK_CLIENT_SECRET,
+      code,
+      grant_type: "authorization_code",
+      redirect_uri: "https://raphaelpro022.github.io/tiktok-site/callback.html"
+    })
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
